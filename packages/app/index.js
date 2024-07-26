3c5d9546d04a8e9758738ca72e6753aea06438ce
zuhairtaha <zt@wizkids.tech>
2024-07-26 22:13:05 +0200

    js to ts

--- packages/app/index.js
+++
@@ -1,5 +0,0 @@
-import { second } from "@tahasoft/ui";
-
-console.log("Hello from main package");
-
-console.log(second);
--- packages/ui/index.js
+++
@@ -1,7 +0,0 @@
-import kebabCase from "just-kebab-case";
-import snakeCase from "just-snake-case";
-
-console.log(kebabCase("Hello World"));
-console.log(snakeCase("Hello from second package"));
-
-export const second = "second";
